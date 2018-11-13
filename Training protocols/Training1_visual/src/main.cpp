920499949dc4e7873d1eb8cf3bd31f843fcb88ed
stensolator <tor.stensola@neuro.fchampalimaud.org>
2018-11-13 14:58:16 +0000

    Added python code for arduino-python com

--- Training protocols/Training1_visual/src/main.cpp
+++ Training protocols/Training1_visual/src/main.cpp
@@ -1,14 +1,24 @@
 //:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: OVERVIEW :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
 // 1. Define trial types
+// --- For visual only, only 6 stimuli, BUT if we want some play on statistics, should perhaps include in this
 // 2. Determine block structure
+// --- Should have several rounds of the trial types   - x3? so blocks of 18 trials?
+// --- Do we want to have a vector that is shuffled to generate random trial type progression?
 // 3. Set up communication with PYTHON
-// 4. 
+// 4. Write a PYTHON script to save (and plot) progress
+// 5. Set up RANDOM function
+// 6. Set up (multi) timer
+// 7. Set up STATE progression
+// 8. Remember to put in a section (ITI) for sending and receiving data
+// 9. Add visual stimulus function
 
 
 //:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: LIBRARY :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
 #include <Arduino.h>
 
 //:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: VARIABLES :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
+const int nTrials = 6;
+
 
 //:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: FUNCTIONS :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
 
