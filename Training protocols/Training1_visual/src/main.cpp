d1401b061a062f3c19ddfd5ca92511310505d19c
stensolator <tor.stensola@neuro.fchampalimaud.org>
2018-11-17 14:34:04 +0000

    Updates

--- Training protocols/Training1_visual/src/main.cpp
+++ Training protocols/Training1_visual/src/main.cpp
@@ -1,23 +1,25 @@
-//:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: OVERVIEW :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
-  // 1. Define trial types
-  // --- For visual only, only 6 stimuli, BUT if we want some play on statistics, should perhaps include in this
-  // 2. Determine block structure
-  // --- Should have several rounds of the trial types   - x3? so blocks of 18 trials?
-  // --- Do we want to have a vector that is shuffled to generate random trial type progression?
-  // 3. Set up communication with PYTHON
-  //TODO: 4. Write a PYTHON script to save (and plot) progress
-  // 5. Set up RANDOM function
-  //TODO: 6. Set up (multi) timer
-  //TODO: 7. Set up STATE progression
-  //TODO: 8. Remember to put in a section (ITI) for sending and receiving data
-  //TODO: 9. Add visual stimulus function
-  //TODO: 10. Set up random wait time between stimulus offset and reward delivery (use map function)
-  //TODO: 11. Key stroke input for start stop
-  //TODO: 12. Add interrupt for lick detector
-  //TODO: 13. You need to deal with millis rollover!!! FIXME: https://www.baldengineer.com/arduino-how-do-you-reset-millis.html
+//:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: OVERVIEW  :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
+  /*
+  1. Define trial types
+  --- For visual only, only 6 stimuli, BUT if we want some play on statistics, should perhaps include in this
+  2. Determine block structure
+  --- Should have several rounds of the trial types   - x3? so blocks of 18 trials?
+  --- Do we want to have a vector that is shuffled to generate random trial type progression?
+  3. Set up communication with PYTHON
+  TODO: 4. Write a PYTHON script to save (and PLOT) progress
+  5. Set up RANDOM function
+  6. Set up (multi) timer
+  7. Set up STATE progression
+  TODO: 8. Remember to put in a section (ITI) for sending and receiving data
+  TODO: 9. Add visual stimulus function
+  10. Set up random wait time between stimulus offset and reward delivery (use map function)
+  11. Key stroke input for start stop
+  12. Add interrupt for lick detector
+  13. You (don't) need to deal with millis rollover!!! 
+    FIXME: https://arduino.stackexchange.com/questions/22994/resetting-millis-and-micros
+  */
 
-
-//:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: LIBRARY :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
+//:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: LIBRARY   :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
   #include <Arduino.h>
   #include <LiquidCrystal.h>
   #include <avr/interrupt.h>          // For random seed gen
@@ -33,6 +35,8 @@
     int outPins[1]; // Needs to be filled with actual output pin IDs (EXCLUDING lick detector pin)
     bool doRun = false;
     int state = 0;
+    int nextstate = 0;
+    bool giveReward;
 
   // ---Lick detector variables
     const int interruptPin = 2; // This lick detector input MUST be either of {2,3,18,19,20,21} for interrupt to work properly
@@ -48,7 +52,8 @@
     const int tITI = 1000;  // Inter-trial interval (ms)
     const int tS1 = 200;    // S1 stimulus duration (ms)
     const int tReward = 20; // Reward duration (ms)
-    const int tRewardWait[2] = {200, 800}; // Random wait range for reward onset after S1 offset (ms)
+    const int tOffsetR[2] = {200, 800}; // Random wait range for reward onset after S1 offset (ms)
+    int tOffset;
 
   // ---f(timer variables)
     unsigned long pTimer;
@@ -59,6 +64,12 @@
     const int nTrialTypes = 6;
     const int nTrialsinBlock = 3;
     int vecBlock[nTrialsinBlock*nTrialTypes]; // Cf setup 2. for generating actual vector
+    int Ntrials = 0;
+    int Nblocks = 0;
+    unsigned int currentTrial = 0;
+
+    // ---Probability distributions
+    unsigned int pReward[nTrialTypes] = {100,100,50,50,0,0};
 
   // ---Random generator variables
     volatile uint32_t seed;  // These two variables can be reused in your program after the
@@ -96,11 +107,13 @@
     seed = seed << 8;
     seed = seed ^ TCNT1L;
     }
-    // Use in setup section 3.:
-    //  trueRandSeed(); // Calls the true random seed generator from above
-    //  randomSeed(seed);
-    // Use in any section:
-    //  // Then just call random() as usual
+    /*
+    Use in setup section 3.:
+      trueRandSeed(); // Calls the true random seed generator from above
+      randomSeed(seed);
+    Use in any section:
+      Just call random() as usual
+    */
   // -----------------------------------------------------------------------------------------------------------
 
   // 2. f(shuffle trial identities)_____________________________________________________________________________
@@ -110,11 +123,13 @@
     *p=*q;
     *q=temp;
     }
-    // Call with:
-    // for (int k = (nTrialsinBlock * nTrialTypes)-1; k >= 0; k--){
-    //  int j = random(k); // Pick a random index from 0 to k
-    //  swap(&vecBlock[k], &vecBlock[j]); // Swap vecBlock[k] with the element at random index
-    // }
+    /*
+    Call with:
+    for (int k = (nTrialsinBlock * nTrialTypes)-1; k >= 0; k--){
+     int j = random(k); // Pick a random index from 0 to k
+     swap(&vecBlock[k], &vecBlock[j]); // Swap vecBlock[k] with the element at random index
+    }
+    */
   // -----------------------------------------------------------------------------------------------------------
 
   // 3. f(background timer)_____________________________________________________________________________________
@@ -131,17 +146,37 @@
     */
   // -----------------------------------------------------------------------------------------------------------
 
-  //:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: ISR :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
-  // Interrupt Service Routine for lick detection_______________________________________________________________
-    void recLick(){
-    lickState = digitalRead(interruptPin);
-    lickCount += 1;
-    vecLick[0][lickCount] = lickState;
-    vecLick[1][lickCount] = millis();
+  // 4. f(state transition)_____________________________________________________________________________________
+    void transition(int n){
+      state = -1;     // Reset state to default
+      nextstate = n;  // Advance state index
+    }
+    /*
+    Needs decleared:
+    int nextstate = 0;
+    int state = 0;
+    */
+  // -----------------------------------------------------------------------------------------------------------
+
+  // 5. f(output pin toggle)____________________________________________________________________________________
+    void output(int ppins[], int n, char direction){
+      for(int i = 0; i < n; i++){
+        digitalWrite(ppins[i], direction);
+      }
     }
   // -----------------------------------------------------------------------------------------------------------
 
-//:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: SETUP :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
+  //:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: ISR :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
+  // Interrupt Service Routine for lick detection_______________________________________________________________
+    void recLick(){
+      lickState = digitalRead(interruptPin);
+      lickCount += 1;
+      vecLick[0][lickCount] = lickState;
+      vecLick[1][lickCount] = millis();
+    }
+  // -----------------------------------------------------------------------------------------------------------
+
+//:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: SETUP     :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
   void setup() {
     // 1. Open serial com
     Serial.begin(rateBaud);
@@ -162,7 +197,11 @@
     }
 
     // 5. Set up interrupt for lick detector
+    pinMode(interruptPin, INPUT_PULLUP); //TODO: check if pullup needed
     attachInterrupt(digitalPinToInterrupt(interruptPin), recLick, CHANGE);
+
+    // 6. Start LCD
+    lcd.begin(16, 2);
 
     // End. Wait with next stage until serial monitor is running
     while (! Serial);
@@ -172,7 +211,7 @@
 
 //:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: MAIN LOOP :_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
   void loop() {
-    // ........Serial communication..........
+  //.............................. SERIAL INPUT ..............................
     if (Serial.available() > 0) {
       // Read the incoming byte
       incomingByte = Serial.read();
@@ -187,55 +226,97 @@
         else{
           Serial.println("Training protocol stopped");
           doRun = false;
+          doTime = false;
         }
       }
     }
+  
+  //.............................. Timer ..............................
+    if(doTime){
+      if (millis() - pTimer >= tTimer){
+          state = nextstate; // Trigger state transition via switch cases below
+          doTime = false; // Reset timer
+      }
+    }
 
+  //.............................. STATE PROGRESSION ..............................
     if (doRun){
       // Start block------------
       switch(state){
-      
       case 0: 
-        // 1. Shuffle block vector to randomize upcoming trials
-        for (k = (nTrialsinBlock * nTrialTypes)-1; k >= 0; k--){
-          j = random(k); // Pick a random index from 0 to k
-          swap(&vecBlock[k], &vecBlock[j]); // Swap vecBlock[k] with the element at random index
+        // 1. Start ITI timer
+        bgtimer(tITI);
+
+        // 2. Define current trial as modulo trial types
+        currentTrial = Ntrials % (nTrialsinBlock * nTrialTypes);
+
+        // 3. Shuffle block vector to randomize upcoming trials IF new block
+        if (currentTrial == 0){
+          for (k = (nTrialsinBlock * nTrialTypes)-1; k >= 0; k--){
+            j = random(k); // Pick a random index from 0 to k
+            swap(&vecBlock[k], &vecBlock[j]); // Swap vecBlock[k] with the element at random index
+          }
+          Nblocks += 1; // Update block counter
         }
 
-        // Send lick data over serial com to python
-        for (i = 0; i < lickCount; i++){
-          Serial.print("Ld"); // Lick event key
-          Serial.print(vecLick[0][i]);
-          Serial.print("Lt"); // Lick timestamp key
-          Serial.print(vecLick[1][i]);
-        }
-        // Reset lick counter
+        // 4. Reset lick counter
         lickCount = 0;
+        
+        // 5. Figure out this trial's random reward time offset
+        tOffset = random(tOffsetR[0],tOffsetR[1]); // Input is [min,max] as defined in tOffsetR
+
+        // 6. Determine this trial's reward outcome 
+        if(random(99) < pReward[vecBlock[currentTrial]]){
+          giveReward = true;
+        }
+        else{
+          giveReward = false;
+        }
+
+        // 7. Update trial counter
+        Ntrials += 1;
+
+        /* ONLY an option to use LCD for this if NOT using LCD for visual stimulation.....
+        // 8. Write to LCD TODO: Set cursor and clear LCD
+        lcd.clear();
+        lcd.print("Block: "); // Need block counter
+        lcd.println(Nblocks);
+        lcd.print("Trial: "); // Need trial counter
+        lcd.print(Nblocks);
+        */
+
+        // 9. Send serial data to python
+          // 1. Lick data
+          for (i = 0; i < lickCount; i++){
+            Serial.print("Ld"); // Lick event key
+            Serial.print(vecLick[0][i]);
+            Serial.print("Lt"); // Lick timestamp key
+            Serial.print(vecLick[1][i]);
+          }
+          // 2. State progression data
+          // 3. Treadmill
+        
+        // 10. Define next transition state
+        transition(4);
+
         break;
       case 1:
-
-      // Define reward outcomes for any trials with uncertainty
-
-      // Check timer
-
-      // Check lick status
-
-      // Send info to python
-
-      // Check for input from key press or python
-
-      // Output/state progression
+      case 2:
+      case 3:
+        bgtimer(tOffset);
+        transition(4);
+        /* Overview
+          case 0: ITI: set time ref first, then block vector generation, random offset, serial port check, 
+            serial data sender, write to LCD, reset lick counter then finally run timer
+          case 1: S1 onset
+          case 2: S1 offset
+          case 3: Timer with random offset
+          case 4: Reward onset
+          case 5: Reward offset
+        */
       default:
       break;
       }
     }
   }
-/* Overview
-  case 0: ITI: set time ref first, then block vector generation, random offset, serial port check, 
-  serial data sender, write to LCD, reset lick counter then finally run timer
-  case 1: S1 onset
-  case 2: S1 offset
-  case 3: Timer with random offset
-  case 4: Reward onset
-  case 5: Reward offset
-*/
+
